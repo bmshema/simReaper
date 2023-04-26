5c725b8397161d23df6258f1581b4d5e31185be5
bmshema <92175742+bmshema@users.noreply.github.com>
2023-04-26 15:57:22 -0500

    Moved non-AT command related function to utils.go

--- commands.go
+++ commands.go
@@ -3,46 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/exec"
-	"runtime"
-	"strings"
 	"time"
 
 	"github.com/tarm/serial"
 )
-
-var clear map[string]func()
-
-// Initialized the clear/cls commands for appropritate runtime env
-func init() {
-	clear = make(map[string]func()) // Initialize
-	clear["linux"] = func() {
-		cmd := exec.Command("clear")
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	}
-}
-
-// Clears terminal window
-func CallClear() {
-	value, ok := clear[runtime.GOOS]
-	if ok {
-		value()
-	} else {
-		panic("Unsupported platform. I can't clear your terminal screen :(")
-	}
-}
-
-// Removes spaces, tabs, newlines and AT echo from responses
-func CleanResponse(s string) string {
-	s = strings.Replace(s, "OK", "", -1)
-	s = strings.Replace(s, "\t", "", -1)
-	s = strings.Replace(s, "\r", "", -1)
-	s = strings.Replace(s, "\n", "", -1)
-	s = strings.Replace(s, "AT+CIMI", "", -1)
-	return s
-}
 
 func ImsiCommand() string {
 	config := &serial.Config{
