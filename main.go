5577763ac165b4a7376cf3c94374a0756e4a95d9
bmshema <92175742+bmshema@users.noreply.github.com>
2023-04-26 17:12:40 -0500

    Removed unnecessary files

--- main.go
+++ main.go
@@ -2,64 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"strings"
-	"time"
-
-	"github.com/tarm/serial"
 )
 
 func main() {
-	test := testCommand()
-	fmt.Println(test)
-}
+	StartCommands()
 
-// Removes spaces, tabs, newlines and AT echo from output
-func cleanUp(s string) string {
-	s = strings.Replace(s, "*AT*", "", -1)
-	s = strings.Replace(s, "\t", "", -1)
-	s = strings.Replace(s, "\n", "", -1)
-	return string(s)
-}
+	fmt.Printf("IMSI:   %s\n", CleanImsi(ImsiCommand()))
 
-func testCommand() string {
-	config := &serial.Config{
-		Name:        "/dev/ttyUSB2",
-		Baud:        115200,
-		ReadTimeout: time.Millisecond * 100,
-		Size:        8,
-		Parity:      serial.ParityNone,
-		StopBits:    serial.Stop1,
-	}
+	fmt.Printf("ICCID: %v\n", CleanIccid(IccidCommand()))
 
-	// Opens the serial port
-	port, err := serial.OpenPort(config)
-	if err != nil {
-		log.Fatal(err)
-		// fmt.Println(err)
-	}
-	defer port.Close()
-
-	// Write AT commands to the modem
-	testCommand := "AT\r\n"
-	_, err = port.Write([]byte(testCommand))
-	if err != nil {
-		fmt.Println("Cannot connect with device...")
-		log.Fatal(err)
-	}
-
-	// Reads modem Responses
-	buffer := make([]byte, 128)
-	n, err := port.Read(buffer)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Print Response
-	response := string(buffer[:n])
-	response = strings.Replace(response, "*AT*", "", -1)
-	response = strings.Replace(response, "\t", "", -1)
-	response = strings.Replace(response, "\n", "", -1)
-	// cleanResp := (cleanUp(response))
-	return response
 }
