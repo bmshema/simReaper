f7517b40c06cbd4ad85b7858e4ca175512a30098
bmshema <92175742+bmshema@users.noreply.github.com>
2023-04-26 15:54:41 -0500

    Moved ImsiCommand over from main.go to a function

--- commands.go
+++ commands.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"runtime"
 	"strings"
+	"time"
+
+	"github.com/tarm/serial"
 )
 
 var clear map[string]func()
 
+// Initialized the clear/cls commands for appropritate runtime env
 func init() {
 	clear = make(map[string]func()) // Initialize
 	clear["linux"] = func() {
@@ -18,6 +24,7 @@ func init() {
 	}
 }
 
+// Clears terminal window
 func CallClear() {
 	value, ok := clear[runtime.GOOS]
 	if ok {
@@ -35,4 +42,42 @@ func CleanResponse(s string) string {
 	s = strings.Replace(s, "\n", "", -1)
 	s = strings.Replace(s, "AT+CIMI", "", -1)
 	return s
+}
+
+func ImsiCommand() string {
+	config := &serial.Config{
+		Name:        "/dev/ttyUSB2",
+		Baud:        115200,
+		ReadTimeout: time.Millisecond * 100,
+		Size:        8,
+		Parity:      serial.ParityNone,
+		StopBits:    serial.Stop1,
+	}
+
+	// Opens the serial port
+	port, err := serial.OpenPort(config)
+	if err != nil {
+		log.Fatal(err)
+		// fmt.Println(err)
+	}
+	defer port.Close()
+
+	// Write AT commands to the modem
+	imsiCommand := "AT+CIMI\r\n"
+	_, err = port.Write([]byte(imsiCommand))
+	if err != nil {
+		fmt.Println("Cannot connect with device...")
+		log.Fatal(err)
+	}
+
+	// Reads modem Responses
+	buffer := make([]byte, 128)
+	n, err := port.Read(buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print Response
+	response := string(buffer[:n])
+	return response
 }
