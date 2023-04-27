db83ef0ffae5cfe8503229bc7922ccee747afb1d
bmshema <92175742+bmshema@users.noreply.github.com>
2023-04-26 20:48:52 -0500

    Added some output formatting

--- main.go
+++ main.go
@@ -6,11 +6,12 @@ import (
 
 func main() {
 	StartCommands()
-
+	fmt.Printf("\nIdentifiers:\n\n")
+	fmt.Printf("Type    Value\n")
+	fmt.Println("---------------------------")
 	fmt.Printf("IMSI:   %s\n", CleanImsi(ImsiCommand()))
-
-	fmt.Printf("ICCID: %v\n\n", CleanIccid(IccidCommand()))
-
+	fmt.Printf("ICCID: %v\n", CleanIccid(IccidCommand()))
+	fmt.Printf("MSISDN: \n\n")
 	fmt.Printf("Contacts:\n %v\n", CleanContacts(GetContacts()))
 
 }
