24fc67e9879f10e50969feca7be7cc287918f2b8
bmshema <92175742+bmshema@users.noreply.github.com>
2023-04-27 13:08:19 -0500

    Added print statements to check msisdn and contacts outputs.

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ func main() {
 	fmt.Printf("\nIdentifiers:\n\n")
 	fmt.Printf("Type    Value\n")
 	fmt.Println("---------------------------")
-	fmt.Printf("IMSI:   %s\n", CleanImsi(ImsiCommand()))
-	fmt.Printf("ICCID: %v\n", CleanIccid(IccidCommand()))
-	fmt.Printf("MSISDN: \n\n")
-	fmt.Printf("Contacts:\n %v\n", CleanContacts(GetContacts()))
-
+	fmt.Printf("IMSI:   %v\n", CleanImsi(GetImsi()))
+	fmt.Printf("ICCID: %v\n", CleanIccid(GetIccid()))
+	fmt.Printf("MSISDN: %v\n\n", CleanMsisdn(GetMsisdn()))
+	fmt.Printf("Contacts:\n\n")
+	fmt.Printf("#  Number        Name\n")
+	fmt.Println("---------------------------")
+	fmt.Println(CleanContacts(GetContacts()))
 }
